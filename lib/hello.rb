8d1d4298fac2fa91e8d22b74d2b1d45fb3687997
Ashlen Mencia <ashmencia0103@gmail.com>
2020-04-24 00:05:55 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,6 +1,19 @@
-def hello_t
-
+array = ["Tim", "Tom", "Jim"]
+  # call your method here!
+def hello_t(array)
+    if name.start_with?("T") 
+    puts "Hi, #{name}"
+    end
+  end
 end
 
-# call your method here!
 
+
+
+# i = place holder for our yielded value
+# i = 0
+#
+# while i < array.length
+#   yield array[i]
+#   i = i + 1
+# end
